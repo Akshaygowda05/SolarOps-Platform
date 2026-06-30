927d86c0f1278b7062b24e585d4ee204f04917cd
Akshaygowda05 <akshay2002gowda@gmail.com>
2026-06-30 22:52:40 +0530

    added tooling for createScedular thing which working needed to work on other tools also

---
+++ userManagmentBackend/src/chripstackfunction/mulitcast.function.ts
@@ -0,0 +1,40 @@
+import apiClient from "../config/apiclient";
+
+export async function getmulticastgroups(applicationId:string,limit:Number) {
+    try {
+        const response = await apiClient.get('/api/multicast-groups', {
+      params: {
+        limit: limit || 100,
+        applicationId: applicationId
+      }
+    });
+
+    return response.data
+    } catch (error) {
+        return error
+    }
+}
+
+export async function serachMulticastSerach(applicationId:string,query:string){
+
+
+    try {
+        const result = await apiClient.get('/api/multicast-groups',{
+             params: {
+        limit: 100,
+        applicationId: applicationId
+      }
+        })
+
+        const groups = result.data.result;
+
+          const matches = groups.filter((group: any) =>
+    group.name.toLowerCase().includes(query.toLowerCase())
+  );
+
+  return matches
+
+    } catch (error) {
+        return  []
+    }
+}
